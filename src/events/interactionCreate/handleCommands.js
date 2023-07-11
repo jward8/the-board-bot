4b2f883896288d71790029ac384aacaae97c7375
Jack Ward <jward8@wisc.edu>
2023-07-11 14:04:09 -0500

    Remove logic entirely

--- src/events/interactionCreate/handleCommands.js
+++ src/events/interactionCreate/handleCommands.js
@@ -14,25 +14,6 @@ module.exports = async (client, interaction) => {
 
         if (!commandObject) return;
 
-        // if (commandObject.devOnly) {
-        //     if(!devs.includes(interaction.member.id)) {
-        //         interaction.reply({
-        //             content: 'Only developers are allowed to run this command.',
-        //             ephemeral: true,
-        //         });
-        //         return;
-        //     }
-        // }
-
-        // if (commandObject.testOnly) {
-        //     if(!interaction.guild.id === testServer) {
-        //         interaction.reply({
-        //             content: 'This command cannot be ran here.',
-        //             ephemeral: true,
-        //         });
-        //         return;
-        //     }
-        // }
         await commandObject.callback(client, interaction);
     } catch (error) {
         console.error(`There was an error running this command: ${error}. at ${__filename}`);
